c96d5d59aa7dd258c1f6e7e885e5a47acd9c7ad3
mehni <lozomehni28@gmail.com>
2023-08-20 18:32:18 +0200

    assumingly ponging

--- app/server.go
+++ app/server.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 )
 
+func requestReponse(bind net.Conn) {
+	name := bind.RemoteAddr().String()
+	fmt.Println("Receiving request from ", name)
+	message := bufio.NewScanner(bind)
+	message.Scan()
+	fmt.Printf("%s: %s\n", name, message.Text())
+	bind.Write([]byte("+PONG\r\n"))
+}
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -15,9 +24,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		bind, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go requestReponse(bind)
 	}
 }
